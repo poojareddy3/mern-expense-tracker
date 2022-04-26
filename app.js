e3cbfe9ffc2a5233756f81f7b139b39b4d5bb0c4
Poojitha Vangala <poojithavangala@Poojithas-MacBook-Pro.local>
2022-04-26 19:22:54 -0400

    some changes

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 //app.use (cors())
-app.use(cors((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-  }));
-//app.use(cors({credentials: true}))
+// app.use(cors((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     next();
+//   }));
+app.use(cors({credentials: true, origin: true}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
