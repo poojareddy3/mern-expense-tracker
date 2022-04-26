195ea5fd51bee77a15cfccf7dfe34b971a37e122
Poojitha Vangala <poojithavangala@Poojithas-MacBook-Pro.local>
2022-04-26 18:49:34 -0400

    some changes

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 //app.use (cors())
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-});
-app.use(cors({credentials: true, origin:true, }))
+// app.use(function (req, res, next) {
+//     //Enabling CORS
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+//       next();
+// });
+app.use(cors({credentials: true}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
